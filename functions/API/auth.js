591880b020869153541aeafc4a1770d11e43f5eb
Barberena Mi Municipio <elozcuro1994@gmail.com>
2026-09-02 15:46:12 -0600

    Update auth.js

--- functions/API/auth.js
+++ functions/API/auth.js
@@ -1,11 +1,11 @@
 export async function onRequest(context) {
     const {
-        request, // same as existing Worker API
-        env, // same as existing Worker API
-        params, // if filename includes [id] or [[path]]
-        waitUntil, // same as ctx.waitUntil in existing Worker API
-        next, // used for middleware or to fetch assets
-        data, // arbitrary space for passing data between middlewares
+        request, 
+        env, 
+        params, 
+        waitUntil, 
+        next, 
+        data, 
     } = context;
 
     const client_id = env.GITHUB_CLIENT_ID;
@@ -14,13 +14,14 @@ export async function onRequest(context) {
         const url = new URL(request.url);
         const redirectUrl = new URL('https://github.com/login/oauth/authorize');
         redirectUrl.searchParams.set('client_id', client_id);
-        redirectUrl.searchParams.set('redirect_uri', url.origin + '/api/callback');
+        // Ajusta la ruta para que incluya /auth/callback
+        redirectUrl.searchParams.set('redirect_uri', url.origin + '/api/auth/callback');
         redirectUrl.searchParams.set('scope', 'repo user');
         redirectUrl.searchParams.set(
             'state',
             crypto.getRandomValues(new Uint8Array(12)).join(''),
         );
-        return Response.redirect(redirectUrl.href, 301);
+        return Response.redirect(redirectUrl.href, 302); // Es mejor usar 302 para redirecciones OAuth temporales
 
     } catch (error) {
         console.error(error);
